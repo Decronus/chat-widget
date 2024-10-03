167fe17fa3c48608e9dc2e5539f740e5eb6c5afd
Alex Zotin <zick3333@mail.ru>
2024-10-03 15:36:16 +0200

    refacto structure

--- scripts/axios.js
+++ scripts/axios.js
@@ -1,5 +1,5 @@
-const $axios = axios.create({
-    baseURL: 'https://panel.makeyourpanel.com/',
-});
-
-export default $axios;
+// const $axios = axios.create({
+//     baseURL: 'https://panel.makeyourpanel.com/',
+// });
+//
+// export default $axios;
--- scripts/fetches.js
+++ scripts/fetches.js
@@ -1,14 +1,14 @@
-import $axios from "/scripts/axios.js";
-import {support} from "/scripts/endpoints.js";
-
-export async function getSupportConversationList() {
-    return await $axios.get(support.conversationList);
-}
-
-export async function getSupportMessageList() {
-    return await $axios.get(support.messageList);
-}
-
-export async function setEmail() {
-    return await $axios.post(support.messageSetEmail);
-}
+// import $axios from "/scripts/axios.js";
+// import {support} from "/scripts/endpoints.js";
+//
+// export async function getSupportConversationList() {
+//     return await $axios.get(support.conversationList);
+// }
+//
+// export async function getSupportMessageList() {
+//     return await $axios.get(support.messageList);
+// }
+//
+// export async function setEmail() {
+//     return await $axios.post(support.messageSetEmail);
+// }
